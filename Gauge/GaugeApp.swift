da0f938eb5fe7303b1a8cd962260e183ae585e57
Shane Claussen <sclaussen@yahoo.com>
2022-03-30 19:50:38 -0700

    Initial Commit

---
+++ Gauge/GaugeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaugeApp.swift
+//  Gauge
+//
+//  Created by Shane Claussen on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct GaugeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
